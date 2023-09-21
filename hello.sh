e5b18256262435d027cfb3e188f2fdab8cf6cf12
Swetha Gupta <swetha.gupta@genesis.global>
2023-09-20 18:08:21 -0700

    Revert "updated hello.sh"
    This reverts commit ff27288fe31e177bcf03525670a60a368032d512.

--- hello.sh
+++ hello.sh
@@ -1,5 +1,3 @@
 echo "Hello World"
 
-echo "updating hello.sh filew.r.t changes to featureB"
-
-echo "updating hello.sh file w.r.t changes to featureC"
+echo "updating hello.sh filw w.r.t changes to featureB"
